2bfb76b296690f7c0b870641659605ba0059e7bb
Torikul Islam Naeem <naeemislam0252@gmail.com>
2026-07-11 00:37:18 +0600

    Fix project image corners and update portfolio icon

--- src/components/CodeMark.jsx
+++ src/components/CodeMark.jsx
@@ -1,3 +1,3 @@
 export default function CodeMark() {
-  return <span className="code-mark" aria-hidden="true"><svg viewBox="0 0 32 32"><rect x="1" y="1" width="30" height="30" rx="9" /><path d="m12.5 10-6 6 6 6M19.5 10l6 6-6 6M18 7l-4 18" /></svg></span>
+  return <span className="code-mark" aria-hidden="true"><img src="/portfolio-icon.png" alt="" /></span>
 }
